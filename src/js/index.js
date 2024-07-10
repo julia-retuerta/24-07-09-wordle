cf714b878e95cfb74b8a4243ac470e7b9e15d7c8
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-07-10 11:59:48 +0200

    clase-10-julio

--- src/js/index.js
+++ src/js/index.js
@@ -3,12 +3,33 @@ import '../scss/styles.scss';
 
 const formElement = document.getElementById('form');
 const inputTypeSolutionElement = document.getElementById('input-type-solution');
-const letterBoxElement = document.getElementById('letter-box');
+const lettersContainerElement = document.getElementById('letters-container');
 
-const typeSolution = event => {
+const SOLUTION_WORDS = ['pan', 'buzo', 'hielo', 'bosque'];
+
+const NUMBER_OF_TRIES = 5;
+
+let secretWord;
+
+const solutionWord = 'hielo';
+
+const wordLength = event => {
   event.preventDefault();
-  if (!event.target.value) return;
+  const userWord = inputTypeSolutionElement.value.toLowerCase();
+  if (inputTypeSolutionElement.length !== 5) {
+    alert('La palabra debe tener 5 letras');
+    return;
+  }
   event.target.reset();
 };
 
-formElement.addEventListener('input', typeSolution);
+const compareWords = (solutionWord, userWord) => {
+  const result = [];
+  for (let i = 0; i < 5; i++) {
+    if (solutionWord.includes(userWord)) {
+      return 'Has acertado la palabra';
+    }
+  }
+};
+
+formElement.addEventListener('submit', wordLength);
