686848ad6d1425e668e987c824d6e2ef4a65e301
juliaret <julia.retuertanogales@alumnostrazos.net>
2024-07-10 09:03:01 +0200

    practica-casa-10-julio

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,14 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const formElement = document.getElementById('form');
+const inputTypeSolutionElement = document.getElementById('input-type-solution');
+const letterBoxElement = document.getElementById('letter-box');
+
+const typeSolution = event => {
+  event.preventDefault();
+  if (!event.target.value) return;
+  event.target.reset();
+};
+
+formElement.addEventListener('input', typeSolution);
